4b3faa54dc7eb6c9445def08c45eb7509b42801e
ronnie pryor <rpryor93@gmail.com>
2016-08-26 23:46:20 -0400

    footer teampage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
     
-    // store our API endpoint
-        var queryURL = "https://api.openweathermap.org/data/2.5/weatherq=orlando&api_key=04b494e287408f88135e3fe897efaf3d";
+    
+        var queryURL = "https://api.forecast.io/forecastc931a56775202db9dee5bf09e73d5511/28.5383°N, 81.3792°W";
 
-        // use ajax to GET the queryUrl
-        $.ajax({url: queryURL, method: 'GET'})
-        
+
         console.log(queryURL);
+    
     
 })
 
-js
